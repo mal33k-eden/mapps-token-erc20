e1999e739bfc980f3bc3c4ae0903475d21e5f19a
Emm Kwami <edenated@gmail.com>
2021-05-04 03:59:29 -0700

    balance for admin allocated and correct details initialized

--- contracts/MappsToken.sol
+++ contracts/MappsToken.sol
@@ -4,12 +4,23 @@ contract MappsToken {
     // Constructor 
     // set the total number of tokens
     // read the total numbe rof tokens 
+    string public name = "Mapps Token";
+    string public symbol = "MAPPS";
+    string public standard = "Mapps Token v1.0";
 
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
 
     constructor() public{
         //totalSuPPLY IS A STATE VARIABLE : MEANING AVAILABLE THROUGHOUT THE CONTRACT 
-        totalSupply = 10000000;
+         _setTotalSupply();
+    }
+
+    function _setTotalSupply() public{
+        totalSupply  = 10000000;
+        //allocate the total supply to the admins wallet
+        balanceOf[msg.sender] = totalSupply; 
 
     }
 }
--- test/MappsToken.test.js
+++ test/MappsToken.test.js
@@ -1,12 +1,32 @@
 var MappsToken = artifacts.require("MappsToken");
 
 contract('MappsToken', function(accounts){
-    it("sets the total supply upon deployment", function(){
+    var tokenInstance;
+    
+
+    it("initializes the contract with the collect values", function(){
+        return MappsToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name,"Mapps Token", "has the correct name");
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol,"MAPPS", "has the correct symbol");
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(symbol,"Mapps Token v1.0", "has the correct standard");
+        });
+    });
+    it("allocates the initial total supply upon deployment", function(){
         return MappsToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
-            assert.equal(totalSupply.toNumber(),10000000, 'Sets the total supply to 10000000')
+            assert.equal(totalSupply.toNumber(),10000000, 'Sets the total supply to 10000000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(), 10000000, "it allocates the initial supply to the admin wallet");
         });
     });
 });
