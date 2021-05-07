6358f7e4ed594d53d224d20ceb08c0b98e26bc4f
Emm Kwami <edenated@gmail.com>
2021-05-07 06:15:25 -0700

    complete build of contract and ui

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,98 @@
+const ethereumButton = document.querySelector('.enableEthereumButton');
 App = {
+    ropstenTestNet:'https://ropsten.infura.io/v3/66d10f25b1974a5383319c94e91fa208',
+    infuraKey:'66d10f25b1974a5383319c94e91fa208',
     web3Provider:null,
     contracts:{},
+    MTCADD:"0x84640Ac4446875915F54C55c2F7c9DB4DE3c8F26",
+    MTSCADD:"0x35295e11F9d13bd01805722e386995767531F23B",
     account:"0x0",
+    admin:"0x0",
     loading: false,
     tokenPrice: 1000000000000000,
     tokensSold: 0,
     tokensAvailable: 7500000,
     init:function(){
         console.log("App Initialized....");
-        return App.loadWeb3();
+        //Load metamask function on load
+        App.requestMetaMaskWallet();
+        App.admin="0xA9B67fc86a5D1F983a3Efe36aa732ce6D441fecB";
+        return  App.loadWeb3();
     },
     loadWeb3: function(){
-        // Specify default instance if no web3 instance provided
-        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-        web3 = new Web3(App.web3Provider);
+      // Specify default instance if no web3 instance provided
+      //App.web3Provider = new Web3.providers.HttpProvider("http://localhost.com:7545");
+      App.web3Provider = new Web3.providers.HttpProvider(App.ropstenTestNet);
+      //App.web3Provider = new Web3HttpProvider(`https://rinkeby.infura.io/v3/${App.infuraKey}`);
+      web3 = new Web3(App.web3Provider);
+      //console.log(web3);
         return App.loadContract();
     },
+    connectToMetaMaskWallet: async function(){
+      await ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then(App.loadMetaMaskWallet)
+      .catch((err) => {
+        
+      });
+    },
+    requestMetaMaskWallet: function(){
+      ethereum.request({ method: 'eth_accounts' })
+            .then(App.loadMetaMaskWallet)
+            .catch((err) => {
+              // Some unexpected error.
+              // For backwards compatibility reasons, if no accounts are available,
+              // eth_accounts will return an empty array.
+              console.error(err);
+              $(".enableEthereumButton").show();
+              ethereumButton.addEventListener('click', () => {
+                //Will Start the metamask extension
+                App.connectToMetaMaskWallet();
+              });
+            });
+    },
+    loadMetaMaskWallet: function(accounts){
+      if(Array.isArray(accounts)){
+        if (accounts.length === 0) {
+          // MetaMask is locked or the user has not connected any accounts
+          console.log('Please connect to MetaMask.');
+              $(".enableEthereumButton").show();
+              ethereumButton.addEventListener('click', () => {
+                //Will Start the metamask extension
+                App.connectToMetaMaskWallet();
+              });
+        } else if (accounts[0] !== App.account) {
+          App.account = accounts[0];
+          // Do any other work!
+          $(".enableEthereumButton").hide();
+          $('#accountAddress').html("your account: " + App.account);
+        }
+      }else if (accounts !== App.account) {
+        App.account = accounts[0];
+        // Do any other work!
+        $(".enableEthereumButton").hide();
+        $('#accountAddress').html("your account: " + App.account);
+      }
+        
+    },
     loadContract:function(){
-        $.getJSON("MappsTokenSale.json", function(mappsTokenSale){
-            App.contracts.MappsTokenSale = TruffleContract(mappsTokenSale);
-            App.contracts.MappsTokenSale.setProvider(App.web3Provider);
-            App.contracts.MappsTokenSale.deployed().then(function(mappsTokenSale){
-                console.log("Mapp Token Sale Address:", mappsTokenSale.address);
-            });
-        }).done(function(){
-            $.getJSON("MappsToken.json", function(mappsToken){
-                App.contracts.MappsToken = TruffleContract(mappsToken);
-                App.contracts.MappsToken.setProvider(App.web3Provider);
-                App.contracts.MappsToken.deployed().then(function(mappsToken){
-                    console.log("Mapp Token  Address:", mappsToken.address);
-                });
-            });
-            App.listenForEvents();
-            return App.render();
-        });
+      App.contracts.MappsToken = new web3.eth.Contract(abi,App.MTCADD);
+      App.contracts.MappsTokenSale = new web3.eth.Contract(saleabi,App.MTSCADD);
+      //App.listenForEvents();
+      return App.render();
     },
     // Listen for events emitted from the contract
   listenForEvents: function() {
-    App.contracts.MappsTokenSale.deployed().then(function(instance) {
-      instance.Sell({}, {
+    App.contracts.MappsTokenSale.events.Sell({
         fromBlock: 0,
         toBlock: 'latest',
-      }).watch(function(error, event) {
-        console.log("event triggered", event);
-        App.render();
-      })
-    })
+    },function(error, event){
+      console.log("error triggered", error);
+      console.log("event triggered", event);
+      App.render();
+    });
   },
-    render: function(){
+  render: function(){
         if (App.loading) {
         return;
         }
@@ -58,62 +103,66 @@ App = {
     
         loader.show();
         content.hide();
-        web3.eth.getAccounts(function(err,account){
-            if(err === null){
-                App.account = account[4];
-                console.log("account : ", App.account);
-                $('#accountAddress').html("your account: " + account[0]);
-            }
+       App.contracts.MappsTokenSale.methods.tokenPrice().call().then((tokenPrice)=>{
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.utils.fromWei(App.tokenPrice, "ether"));
+            $('.mapps-price').html(web3.utils.fromWei(App.tokenPrice, "ether"));
+            return App.contracts.MappsTokenSale.methods.tokensSold().call();
+        }).then((tokensSold)=>{
+            App.tokensSold = tokensSold;
+            $('.tokens-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+            var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%');
+
+            return  App.contracts.MappsToken.methods.balanceOf(App.account).call();
+        }).then((balance)=>{
+          console.log(balance);
+          $('.mapps-balance').html(balance);
         });
-       // Load token sale contract
-    App.contracts.MappsTokenSale.deployed().then(function(instance) {
-        mappsTokenSaleInstance = instance;
-        return mappsTokenSaleInstance.tokenPrice();
-      }).then(function(tokenPrice) {
-        App.tokenPrice = tokenPrice;
-        $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
-        return mappsTokenSaleInstance.tokensSold();
-      }).then(function(tokensSold) {
-        App.tokensSold = tokensSold.toNumber();
-        $('.tokens-sold').html(App.tokensSold);
-        $('.tokens-available').html(App.tokensAvailable);
-  
-        var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
-        $('#progress').css('width', progressPercent + '%');
-  
-        // Load token contract
-        App.contracts.MappsToken.deployed().then(function(instance) {
-            mappsTokenInstance = instance;
-          return mappsTokenInstance.balanceOf(App.account);
-        }).then(function(balance) {
-            console.log(balance.toNumber());
-          $('.mapps-balance').html(balance.toNumber());
-          App.loading = false;
+        App.loading = false;
           loader.hide();
           content.show();
-        })
-      });
-    },
-    buyTokens: function() {
+  },
+  buildTransaction:function(){
+    var numberOfTokens = $('#numberOfTokens').val();
+    var encoded_tx = App.contracts.MappsTokenSale.methods
+                .buyTokens(numberOfTokens).encodeABI();
+                let transactionObject = {
+                  gas: 500000,
+                  data: encoded_tx,
+                  from: ethereum.selectedAddress,
+                  to: App.MTSCADD,
+                  value: numberOfTokens * App.tokenPrice,
+              };
+    return transactionObject;
+  },
+    buyTokens:async  function() {
         $('#content').hide();
         $('#loader').show();
-        var numberOfTokens = $('#numberOfTokens').val();
-        App.contracts.MappsTokenSale.deployed().then(function(instance) {
-          return instance.buyTokens(numberOfTokens, {
-            from: App.account,
-            value: numberOfTokens * App.tokenPrice,
-            gas: 500000 // Gas limit
-          });
-        }).then(function(result) {
-          console.log("Tokens bought...")
-          $('form').trigger('reset') // reset number of tokens in form
-          // Wait for Sell event
-        });
+        web3.eth.accounts.signTransaction(
+          App.buildTransaction(),
+           'PRIVATE KEY GOES HERE',function(e,r){
+                          if (e) {
+                            console.log(e);
+                            // handle error
+                        } else {
+                            web3.eth.sendSignedTransaction(r.rawTransaction).on('receipt', function (receipt) {
+                                //do something
+                                console.log(receipt);
+                        });
+                      }
+           });
+        
+
+                      // .then(function(result) {
+                      //   console.log(result)
+                      //   $('form').trigger('reset') // reset number of tokens in form
+                      //   // Wait for Sell event
+                      // });
       }
 };
 
 $(()=>{
-    $(window).load(()=>{
-        App.init();
-    })
-})
+  App.init();
+});
---
+++ src/js/mtabi.js
@@ -0,0 +1,349 @@
+var abi =[
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "standard",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "_setTotalSupply",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+var saleabi =[
+  {
+    "inputs": [
+      {
+        "internalType": "contract MappsToken",
+        "name": "_tokenContract",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_tokenPrice",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "_buyer",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "Sell",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "tokenContract",
+    "outputs": [
+      {
+        "internalType": "contract MappsToken",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "tokenPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "tokensSold",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_numberOfTokens",
+        "type": "uint256"
+      }
+    ],
+    "name": "buyTokens",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function",
+    "payable": true
+  },
+  {
+    "inputs": [],
+    "name": "endSale",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "shutdownICO",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
